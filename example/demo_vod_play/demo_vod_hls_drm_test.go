ea5d07dd3e3d2bf168aa5de57d1e939dd3541856
leiyixing <leiyixing@bytedance.com>
2021-03-29 21:07:50 +0800

    fix: fix demo bug

--- example/demo_vod_play/demo_vod_hls_drm_test.go
+++ example/demo_vod_play/demo_vod_hls_drm_test.go
@@ -3,7 +3,6 @@ package demo_vod_play
 import (
 	"fmt"
 	"testing"
-	"time"
 
 	"github.com/volcengine/volc-sdk-golang/service/vod"
 )
@@ -18,7 +17,7 @@ func TestVod_GetSha1HlsDrmAuthToken(t *testing.T) {
 	// or set ak and ak as follow
 	//instance.SetAccessKey("your ak")
 	//instance.SetSecretKey("your sk")
-	expireDuration := int64(600 * time.Second) //change to your expire duration (s), no default duration
+	expireDuration := int64(6000000) //change to your expire duration (s), no default duration
 	token, _ := instance.CreateSha1HlsDrmAuthToken(expireDuration)
 	fmt.Println(token)
 }
