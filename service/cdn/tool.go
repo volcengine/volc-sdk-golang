83d104b997e9e12d3c189d49c4d5d4d6b750b8bb
zhouyiheng.go <zhouyiheng.go@bytedance.com>
2022-01-28 11:22:25 +0800

    feat: ValidateResponse method for CDN service

--- service/cdn/tool.go
+++ service/cdn/tool.go
@@ -1,5 +1,11 @@
 package cdn
 
+import "github.com/volcengine/volc-sdk-golang/base"
+
 func (s *CDN) SendCommonRequest(action string, in interface{}, out interface{}) error {
 	return s.post(action, in, out)
+}
+
+func (s *CDN) ValidateResponse(meta *base.ResponseMetadata) error {
+	return validateResponse(meta)
 }
