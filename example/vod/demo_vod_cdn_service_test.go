d4f67468b2a47f473ef8b1d33f13c4eb28854bbf
leiyixing <leiyixing@bytedance.com>
2022-04-26 00:23:33 +0800

    fix: add bindwidth-usage api

--- example/vod/demo_vod_cdn_service_test.go
+++ example/vod/demo_vod_cdn_service_test.go
@@ -91,3 +91,44 @@ func Test_ListCdnTasks(t *testing.T) {
 	fmt.Println(err)
 	fmt.Println(resp.String())
 }
+
+func Test_ListCdnAccessLog(t *testing.T) {
+	instance := vod.NewInstance()
+	instance.SetCredential(base.Credentials{
+		AccessKeyID:     "your ak",
+		SecretAccessKey: "your sk",
+	})
+
+	query := &request.VodListCdnAccessLogRequest{
+		Domains:        "your Domains",
+		StartTimestamp: 0,
+		EndTimestamp:   0,
+		SpaceName:      "your SpaceName",
+	}
+
+	resp, status, err := instance.ListCdnAccessLog(query)
+	fmt.Println(status)
+	fmt.Println(err)
+	fmt.Println(resp.String())
+}
+
+func Test_DescribeVodDomainBandwidthData(t *testing.T) {
+	instance := vod.NewInstance()
+	instance.SetCredential(base.Credentials{
+		AccessKeyID:     "your ak",
+		SecretAccessKey: "your sk",
+	})
+
+	query := &request.VodDescribeVodDomainBandwidthDataRequest{
+		DomainList:    "your DomainList",
+		StartTime:     "your StartTime",
+		EndTime:       "your EndTime",
+		Aggregation:   0,
+		BandwidthType: "your BandwidthType",
+	}
+
+	resp, status, err := instance.DescribeVodDomainBandwidthData(query)
+	fmt.Println(status)
+	fmt.Println(err)
+	fmt.Println(resp.String())
+}
