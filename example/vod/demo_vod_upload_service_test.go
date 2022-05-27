120b5602e77be535ca5444b7f9e307a18efed59e
yanggang.james <yanggang.james@bytedance.com>
2022-05-27 18:48:19 +0800

    feat: 神农计划，添加上传部分的注释

--- example/vod/demo_vod_upload_service_test.go
+++ example/vod/demo_vod_upload_service_test.go
@@ -6,6 +6,7 @@ package vod
 
 import (
 	"fmt"
+	"github.com/volcengine/volc-sdk-golang/service/vod/models/business"
 	"testing"
 
 	"github.com/volcengine/volc-sdk-golang/base"
@@ -20,15 +21,33 @@ func Test_UploadMediaByUrl(t *testing.T) {
 		SecretAccessKey: "your sk",
 	})
 
+	urlSets := make([]*business.VodUrlUploadURLSet, 0)
+	urlSet := &business.VodUrlUploadURLSet{
+		SourceUrl:        "", // 源视频URL
+		CallbackArgs:     "", // 希望回调时会透传返回的信息，最大长度 512字节，非必须字段。
+		Md5:              "", // 视频文件的 Md5 值，如添加则会进行 Md5 的校验，非必须字段
+		TemplateId:       "", // 如希望上传后自动启动工作流，则填写工作流 Id，非必须字段
+		Title:            "", // 标题，不超过 128个字符，非必须字段
+		Description:      "", // 描述，不超过 256个字符，非必须字段
+		Tags:             "", // 标签，多个以“,”分隔，不超过 36个字符，非必须字段
+		FileName:         "", //
+		ClassificationId: 0,  // 分类 ID，可上传时指定分类，非必须字段
+	}
+	urlSets = append(urlSets, urlSet)
+
 	query := &request.VodUrlUploadRequest{
 		SpaceName: "your SpaceName",
-		URLSets:   nil,
+		URLSets:   urlSets,
 	}
 
 	resp, status, err := instance.UploadMediaByUrl(query)
 	fmt.Println(status)
 	fmt.Println(err)
 	fmt.Println(resp.String())
+
+	fmt.Println(resp.GetResponseMetadata().GetRequestId())    // requestId 用于辅助查询
+	fmt.Println(resp.GetResult().GetData()[0].GetSourceUrl()) // 原 url
+	fmt.Println(resp.GetResult().GetData()[0].GetJobId())     // url 对应的 JobId，可用来临时查询视频状态信息
 }
 
 func Test_QueryUploadTaskInfo(t *testing.T) {
@@ -46,45 +65,11 @@ func Test_QueryUploadTaskInfo(t *testing.T) {
 	fmt.Println(status)
 	fmt.Println(err)
 	fmt.Println(resp.String())
-}
+	// 字段解释均可在火山引擎点播官网 开发者API-媒资上传-查询URL批量上传任务状态中找到相关解释
 
-func Test_ApplyUploadInfo(t *testing.T) {
-	instance := vod.NewInstance()
-	instance.SetCredential(base.Credentials{
-		AccessKeyID:     "your ak",
-		SecretAccessKey: "your sk",
-	})
+	fmt.Println(resp.GetResult().GetData().GetMediaInfoList()[0].GetJobId())                    // 可以查看到此记录的 JobId
+	fmt.Println(resp.GetResult().GetData().GetMediaInfoList()[0].GetVid())                      // 此 JobId 对应的视频 Vid
+	fmt.Println(resp.GetResult().GetData().GetMediaInfoList()[0].GetSourceInfo().GetStoreUri()) // 此视频存储 uri
+	fmt.Println(resp.GetResult().GetData().GetMediaInfoList()[0].GetSourceInfo().GetSize())     // 此视频的大小，单位字节
 
-	query := &request.VodApplyUploadInfoRequest{
-		SpaceName:  "your SpaceName",
-		SessionKey: "your SessionKey",
-		FileSize:   0,
-		FileType:   "your FileType",
-		FileName:   "your FileName",
-	}
-
-	resp, status, err := instance.ApplyUploadInfo(query)
-	fmt.Println(status)
-	fmt.Println(err)
-	fmt.Println(resp.String())
-}
-
-func Test_CommitUploadInfo(t *testing.T) {
-	instance := vod.NewInstance()
-	instance.SetCredential(base.Credentials{
-		AccessKeyID:     "your ak",
-		SecretAccessKey: "your sk",
-	})
-
-	query := &request.VodCommitUploadInfoRequest{
-		SpaceName:    "your SpaceName",
-		SessionKey:   "your SessionKey",
-		CallbackArgs: "your CallbackArgs",
-		Functions:    "your Functions",
-	}
-
-	resp, status, err := instance.CommitUploadInfo(query)
-	fmt.Println(status)
-	fmt.Println(err)
-	fmt.Println(resp.String())
 }
