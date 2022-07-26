fcaeedf2fea9a6e6cbf4d4b8cf432727e5a7c12d
zhengjiateng.123 <zhengjiateng.123@bytedance.com>
2022-07-26 14:22:17 +0800

    增加部分云导播接口

---
+++ service/live/caster_model.go
@@ -0,0 +1,347 @@
+package live
+
+import (
+	"github.com/volcengine/volc-sdk-golang/base"
+)
+
+type (
+	EResourceUsed  int
+	EResourceType  int
+	EResourceCate  int
+	EResourceForm  string
+	ERTCStatus     int
+	ERTCUserID     string
+	EScene         int
+	ECasterStatus  int
+	CasterTaskCode int
+)
+
+const (
+	CasterInit    ECasterStatus = 1
+	CasterRunning ECasterStatus = 2
+	CasterFinish  ECasterStatus = 3
+
+	SceneLayout EScene = 0
+	ScenePVW    EScene = 1
+	ScenePGM    EScene = 2
+
+	ResourceNoneUsed   EResourceUsed = 0
+	ResourceLayoutUsed EResourceUsed = 1
+	ResourcePVWUsed    EResourceUsed = 2
+	ResourcePGMUsed    EResourceUsed = 3
+
+	ResourceTypePull            EResourceType = 1
+	ResourceTypePush            EResourceType = 2
+	ResourceTypeVideo           EResourceType = 3
+	ResourceTypeVideoURL        EResourceType = 4
+	ResourceTypePicture         EResourceType = 5
+	ResourceTypePushMate        EResourceType = 6
+	ResourceTypeImageCollection EResourceType = 7
+	ResourceTypeOPED            EResourceType = 8
+
+	ResourceCateVideo           EResourceCate = 1
+	ResourceCatePicture         EResourceCate = 2
+	ResourceCateText            EResourceCate = 3
+	ResourceCateImageCollection EResourceCate = 4
+	ResourceCateOPED            EResourceCate = 5
+
+	RTCStatusNone ERTCStatus = 0
+	RTCStatusOk   ERTCStatus = 1
+	RTCStatusErr  ERTCStatus = 2
+
+	ResourceFormNone EResourceForm = ""
+	ResourceFormRts  EResourceForm = "rts"
+	ResourceFormFlv  EResourceForm = "flv"
+
+	CasterTaskCodeSuccess    CasterTaskCode = 0
+	CasterTaskCodeInit       CasterTaskCode = 1
+	CasterTaskCodeStop       CasterTaskCode = 2
+	CasterTaskCodeNotCreated CasterTaskCode = 3
+	CasterTaskCodeStartField CasterTaskCode = 500
+
+	CloudCastDownloadStatusProcessing = 1
+	CloudCastDownloadStatusSuc        = 2
+	CloudCastDownloadStatusFail       = 3
+)
+
+type CreateCasterResp struct {
+	ResponseMetadata base.ResponseMetadata
+	Result           *CreateCasterResponse `json:"Result,omitempty"`
+}
+
+type CreateCasterResponse struct {
+	CasterID       *int64
+	BackupCasterID *int64
+}
+
+type ListCastersResp struct {
+	ResponseMetadata base.ResponseMetadata
+	Result           *ListCastersResponse `json:"Result,omitempty"`
+}
+
+type ListCastersResponse struct {
+	Casters []*CasterBaseCondition
+	Count   int64
+}
+
+type GetCasterResourceResp struct {
+	ResponseMetadata base.ResponseMetadata
+	Result           *GetCasterResourceResponse `json:"Result,omitempty"`
+}
+
+type GetCasterResourceResponse struct {
+	Resources *CasterInfoResources
+}
+
+type GetCasterLayoutResp struct {
+	ResponseMetadata base.ResponseMetadata
+	Result           *GetCasterLayoutResponse `json:"Result,omitempty"`
+}
+
+type GetCasterLayoutResponse struct {
+	Layouts    []*CasterLayoutCondition
+	LayoutTpls []*CasterLayoutTplCondition
+}
+
+type GetCasterConfigResp struct {
+	ResponseMetadata base.ResponseMetadata
+	Result           *GetCasterConfigResponse `json:"Result,omitempty"`
+}
+
+type GetCasterConfigResponse struct {
+	Caster     *CasterCondition
+	PushParams *PushParams
+}
+
+type StartCasterResp struct {
+	ResponseMetadata base.ResponseMetadata
+}
+
+type StopCasterResp struct {
+	ResponseMetadata base.ResponseMetadata
+}
+
+type CreateCasterResourceResp struct {
+	ResponseMetadata base.ResponseMetadata
+	Result           *CasterResourceCondition `json:"Result,omitempty"`
+}
+
+type UpdateCasterLayoutResp struct {
+	ResponseMetadata base.ResponseMetadata
+}
+
+type DeleteCasterLayoutResp struct {
+	ResponseMetadata base.ResponseMetadata
+}
+
+type CreateCasterResourceImageResp struct {
+	ResponseMetadata base.ResponseMetadata
+}
+
+type DeleteCasterResourceImageResp struct {
+	ResponseMetadata base.ResponseMetadata
+}
+
+type CreateCasterResourceImagesResp struct {
+	ResponseMetadata base.ResponseMetadata
+}
+
+type RemoveCasterResourceResp struct {
+	ResponseMetadata base.ResponseMetadata
+}
+
+type UpdateCasterConfigResp struct {
+	ResponseMetadata base.ResponseMetadata
+}
+
+type CreateCasterResourceOPEDResp struct {
+	ResponseMetadata base.ResponseMetadata
+}
+
+type DeleteCasterResourceOPEDResp struct {
+	ResponseMetadata base.ResponseMetadata
+}
+
+type CasterBaseCondition struct {
+	ID            int64
+	CloudCasterID string
+	IsBackup      bool
+	Name          string
+	Status        ECasterStatus
+	CreateTime    int64
+	UpdateTime    int64
+	Creator       string
+}
+
+type CasterCondition struct {
+	Name          string
+	ID            int64
+	UID           string
+	CloudCasterID string
+	Status        ECasterStatus
+	HasBackup     bool
+	CreateTime    int64
+	UpdateTime    int64
+}
+
+type PushParams struct {
+	Direction int
+	Size      int
+	Fps       int
+	VBitRate  int
+	Delay     int
+	SEI       string
+	SEIs      []string
+	Logo      Logo
+	Gop       int
+	ABitRate  int
+	AChannel  string
+	Codec     string
+	Profile   string
+	ASample   int
+	Tune      string
+	Options   string
+	Streams   []*Stream
+}
+
+type Logo struct {
+	URL string
+	X   int
+	Y   int
+	W   int
+	H   int
+}
+
+type Stream struct {
+	ID  int64
+	URL string
+}
+
+type CasterResourceCondition struct {
+	ID                  int64
+	Name                string
+	No                  int
+	Type                int
+	URL                 string
+	Volume              int
+	Duration            int
+	BreakAt             int
+	Mute                bool
+	VodHeadSwitch       bool
+	VideoUsed           int
+	AudioUsed           int
+	RtcPushUserID       string
+	Status              int
+	PGMUsed             bool
+	PVWUsed             bool
+	AdjustDts           int64
+	AdjustAudio         int
+	AdjustRtcPushUserID string
+	VodUseDownload      bool
+}
+
+type CasterLayoutCondition struct {
+	ID       int64
+	Name     string
+	TplId    int64
+	Elements []*Element
+	PVWUsed  *bool
+	PGMUsed  *bool
+	BackupID int64
+}
+
+type CasterLayoutTplCondition struct {
+	ID         int64
+	Name       string
+	Elements   []*Element
+	W          int
+	H          int
+	ScreenType int
+	CustomType int
+}
+
+type Element struct {
+	Name         string
+	W            float32
+	H            float32
+	X            float32
+	Y            float32
+	ResourceID   int
+	ResourceNO   int
+	Opacity      int
+	ZIndex       int
+	Cate         int
+	IsHide       bool
+	ImageIndexID int64
+}
+
+type CasterInfoResources struct {
+	Video            []*Video
+	Image            []*Image
+	ImageCollections []*ImageCollections
+	OPED             []*OPED
+}
+
+type Video struct {
+	ID                  int64
+	Name                string
+	No                  int64
+	Type                EResourceType
+	URL                 string
+	Volume              int
+	Duration            int
+	BreakAt             int64
+	Mute                bool
+	VodHeadSwitch       bool
+	VideoUsed           EResourceUsed
+	AudioUsed           EResourceUsed
+	RtcPushUserID       ERTCUserID
+	Status              ERTCStatus
+	PGMUsed             bool
+	PVWUsed             bool
+	AdjustDts           int64
+	AdjustAudio         int
+	AdjustRtcPushUserID ERTCUserID
+	VodUseDownload      bool
+}
+
+type Image struct {
+	ID      int64
+	ImageID string
+	Name    string
+	URL     string
+	PGMUsed bool
+	PVWUsed bool
+}
+
+type ImageCollections struct {
+	ID              int64
+	Name            string
+	ImageCollection []ImageCollection
+	DownloadStatus  int
+	DownloadPercent float32
+}
+
+type ImageCollection struct {
+	ImageIndexID   int64
+	ImageID        string
+	URL            string
+	DownloadStatus int
+}
+
+type OPED struct {
+	ID          int64
+	Name        string
+	URL         string
+	PVWUsed     bool
+	PGMUsed     bool
+	Duration    int
+	ArrangeInfo ArrangeInfo
+}
+
+type ArrangeInfo struct {
+	OpType     int
+	ResourceNo int64
+	ResourceID int64
+	LayoutID   int64
+}
---
+++ service/live/caster_wrapper_test.go
@@ -0,0 +1,415 @@
+package live
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestCreateCaster(t *testing.T) {
+	DefaultInstance.Client.SetAccessKey(testAk)
+	DefaultInstance.Client.SetSecretKey(testSk)
+	bodyMap := map[string]interface{}{
+		"Name":              "test1",
+		"MainStreamURL":     "vhost",
+		"BackupStreamURL":   "test",
+		"Provider":          "test",
+		"StreamExpiredTime": 0,
+	}
+	body, _ := json.Marshal(bodyMap)
+	//打印请求参数
+	t.Logf(string(body))
+	resp, statusCode, err := DefaultInstance.CreateCaster(nil, string(body))
+	if err != nil {
+		t.Logf("error occur %v", err)
+	}
+	res, _ := json.Marshal(resp)
+	t.Logf("statusCode = %+v  msgInfo = %+v \n", statusCode, string(res))
+}
+
+func TestListCasters(t *testing.T) {
+	DefaultInstance.Client.SetAccessKey(testAk)
+	DefaultInstance.Client.SetSecretKey(testSk)
+	bodyMap := map[string]interface{}{
+		"Status": 1,
+	}
+	body, _ := json.Marshal(bodyMap)
+	//打印请求参数
+	t.Logf(string(body))
+	resp, statusCode, err := DefaultInstance.ListCasters(nil, string(body))
+	if err != nil {
+		t.Logf("error occur %v", err)
+	}
+	res, _ := json.Marshal(resp)
+	t.Logf("statusCode = %+v  msgInfo = %+v \n", statusCode, string(res))
+}
+
+func TestGetCasterResource(t *testing.T) {
+	DefaultInstance.Client.SetAccessKey(testAk)
+	DefaultInstance.Client.SetSecretKey(testSk)
+	bodyMap := map[string]interface{}{
+		"CasterID": 0,
+	}
+	body, _ := json.Marshal(bodyMap)
+	//打印请求参数
+	t.Logf(string(body))
+	resp, statusCode, err := DefaultInstance.GetCasterResource(nil, string(body))
+	if err != nil {
+		t.Logf("error occur %v", err)
+	}
+	res, _ := json.Marshal(resp)
+	t.Logf("statusCode = %+v  msgInfo = %+v \n", statusCode, string(res))
+}
+
+func TestGetCasterLayout(t *testing.T) {
+	DefaultInstance.Client.SetAccessKey(testAk)
+	DefaultInstance.Client.SetSecretKey(testSk)
+	bodyMap := map[string]interface{}{
+		"CasterID": 0,
+	}
+	body, _ := json.Marshal(bodyMap)
+	//打印请求参数
+	t.Logf(string(body))
+	resp, statusCode, err := DefaultInstance.GetCasterLayout(nil, string(body))
+	if err != nil {
+		t.Logf("error occur %v", err)
+	}
+	res, _ := json.Marshal(resp)
+	t.Logf("statusCode = %+v  msgInfo = %+v \n", statusCode, string(res))
+}
+
+func TestGetCasterConfig(t *testing.T) {
+	DefaultInstance.Client.SetAccessKey(testAk)
+	DefaultInstance.Client.SetSecretKey(testSk)
+	bodyMap := map[string]interface{}{
+		"CasterID": 0,
+	}
+	body, _ := json.Marshal(bodyMap)
+	//打印请求参数
+	t.Logf(string(body))
+	resp, statusCode, err := DefaultInstance.GetCasterConfig(nil, string(body))
+	if err != nil {
+		t.Logf("error occur %v", err)
+	}
+	res, _ := json.Marshal(resp)
+	t.Logf("statusCode = %+v  msgInfo = %+v \n", statusCode, string(res))
+}
+
+func TestStartCaster(t *testing.T) {
+	DefaultInstance.Client.SetAccessKey(testAk)
+	DefaultInstance.Client.SetSecretKey(testSk)
+	bodyMap := map[string]interface{}{
+		"CasterID": 0,
+	}
+	body, _ := json.Marshal(bodyMap)
+	//打印请求参数
+	t.Logf(string(body))
+	resp, statusCode, err := DefaultInstance.StartCaster(nil, string(body))
+	if err != nil {
+		t.Logf("error occur %v", err)
+	}
+	res, _ := json.Marshal(resp)
+	t.Logf("statusCode = %+v  msgInfo = %+v \n", statusCode, string(res))
+}
+
+func TestStopCaster(t *testing.T) {
+	DefaultInstance.Client.SetAccessKey(testAk)
+	DefaultInstance.Client.SetSecretKey(testSk)
+	bodyMap := map[string]interface{}{
+		"CasterID": 0,
+	}
+	body, _ := json.Marshal(bodyMap)
+	//打印请求参数
+	t.Logf(string(body))
+	resp, statusCode, err := DefaultInstance.StopCaster(nil, string(body))
+	if err != nil {
+		t.Logf("error occur %v", err)
+	}
+	res, _ := json.Marshal(resp)
+	t.Logf("statusCode = %+v  msgInfo = %+v \n", statusCode, string(res))
+}
+
+func TestCreateCasterResource(t *testing.T) {
+	DefaultInstance.Client.SetAccessKey(testAk)
+	DefaultInstance.Client.SetSecretKey(testSk)
+	bodyMap := map[string]interface{}{
+		"CasterID": 0,
+		"Resource": map[string]interface{}{
+			"No":             3,
+			"Type":           7,
+			"URL":            "http://test.example.com/testMp4",
+			"VodUseDownload": true,
+		},
+	}
+	body, _ := json.Marshal(bodyMap)
+	resp, statusCode, err := DefaultInstance.CreateCasterResource(nil, string(body))
+	if err != nil {
+		t.Logf("error occur %v", err)
+	}
+	res, _ := json.Marshal(resp)
+	t.Logf("statusCode = %+v  msgInfo = %+v \n", statusCode, string(res))
+}
+
+func TestRemoveCasterResource(t *testing.T) {
+	DefaultInstance.Client.SetAccessKey(testAk)
+	DefaultInstance.Client.SetSecretKey(testSk)
+	bodyMap := map[string]interface{}{
+		"CasterID":   0,
+		"ResourceID": 0,
+	}
+	body, _ := json.Marshal(bodyMap)
+	//打印请求参数
+	t.Logf(string(body))
+	resp, statusCode, err := DefaultInstance.RemoveCasterResource(nil, string(body))
+	if err != nil {
+		t.Logf("error occur %v", err)
+	}
+	res, _ := json.Marshal(resp)
+	t.Logf("statusCode = %+v  msgInfo = %+v \n", statusCode, string(res))
+}
+
+func TestUpdateCasterLayout(t *testing.T) {
+	DefaultInstance.Client.SetAccessKey(testAk)
+	DefaultInstance.Client.SetSecretKey(testSk)
+	bodyMap := map[string]interface{}{
+		"CasterID": 0,
+		"LayoutID": 0,
+		"Name":     "布局3(横)",
+		"Elements": []map[string]interface{}{
+			{
+				"Name":       "A",
+				"W":          200,
+				"H":          112.5,
+				"X":          0,
+				"Y":          56.25,
+				"Opacity":    100,
+				"ZIndex":     1,
+				"ResourceID": 4359,
+				"ResourceNO": 1,
+			},
+			{
+				"Name":       "B",
+				"W":          200,
+				"H":          225,
+				"X":          200,
+				"Y":          0,
+				"Opacity":    100,
+				"ZIndex":     2,
+				"ResourceID": 4801,
+				"ResourceNO": 2,
+			},
+		},
+		"W":             400,
+		"H":             225,
+		"LayoutTplID":   2,
+		"ScreenType":    1,
+		"SyncLayoutTpl": false,
+		"LayoutTplElements": []map[string]interface{}{
+			{
+				"Name":    "A",
+				"W":       200,
+				"H":       112.5,
+				"X":       0,
+				"Y":       56.25,
+				"Opacity": 100,
+				"ZIndex":  1,
+			},
+			{
+				"Name":    "B",
+				"W":       200,
+				"H":       225,
+				"X":       200,
+				"Y":       0,
+				"Opacity": 100,
+				"ZIndex":  2,
+			},
+		},
+	}
+	body, _ := json.Marshal(bodyMap)
+	//打印请求参数
+	t.Logf(string(body))
+	resp, statusCode, err := DefaultInstance.UpdateCasterLayout(nil, string(body))
+	if err != nil {
+		t.Logf("error occur %v", err)
+	}
+	res, _ := json.Marshal(resp)
+	t.Logf("statusCode = %+v  msgInfo = %+v \n", statusCode, string(res))
+}
+
+func TestDeleteCasterLayout(t *testing.T) {
+	DefaultInstance.Client.SetAccessKey(testAk)
+	DefaultInstance.Client.SetSecretKey(testSk)
+	bodyMap := map[string]interface{}{
+		"CasterID": 0,
+		"LayoutID": 0,
+	}
+	body, _ := json.Marshal(bodyMap)
+	//打印请求参数
+	t.Logf(string(body))
+	resp, statusCode, err := DefaultInstance.DeleteCasterLayout(nil, string(body))
+	if err != nil {
+		t.Logf("error occur %v", err)
+	}
+	res, _ := json.Marshal(resp)
+	t.Logf("statusCode = %+v  msgInfo = %+v \n", statusCode, string(res))
+}
+
+func TestCreateCasterResourceImage(t *testing.T) {
+	DefaultInstance.Client.SetAccessKey(testAk)
+	DefaultInstance.Client.SetSecretKey(testSk)
+	bodyMap := map[string]interface{}{
+		"CasterID": 0,
+		"Resource": map[string]interface{}{
+			"ImageId": "test1",
+			"URL":     "http://test.example.com/test1.png",
+			"Name":    "LWScreenShot 2022-04-20 at 13",
+		},
+		"BackResource": map[string]interface{}{
+			"ImageId": "test1",
+			"URL":     "http://test.example.com/test1.png",
+			"Name":    "LWScreenShot 2022-04-20 at 13",
+		},
+	}
+	body, _ := json.Marshal(bodyMap)
+	//打印请求参数
+	t.Logf(string(body))
+	resp, statusCode, err := DefaultInstance.CreateCasterResourceImage(nil, string(body))
+	if err != nil {
+		t.Logf("error occur %v", err)
+	}
+	res, _ := json.Marshal(resp)
+	t.Logf("statusCode = %+v  msgInfo = %+v \n", statusCode, string(res))
+}
+
+func TestDeleteCasterResourceImage(t *testing.T) {
+	DefaultInstance.Client.SetAccessKey(testAk)
+	DefaultInstance.Client.SetSecretKey(testSk)
+	bodyMap := map[string]interface{}{
+		"CasterID":   0,
+		"ResourceID": 0,
+	}
+	body, _ := json.Marshal(bodyMap)
+	//打印请求参数
+	t.Logf(string(body))
+	resp, statusCode, err := DefaultInstance.DeleteCasterResourceImage(nil, string(body))
+	if err != nil {
+		t.Logf("error occur %v", err)
+	}
+	res, _ := json.Marshal(resp)
+	t.Logf("statusCode = %+v  msgInfo = %+v \n", statusCode, string(res))
+}
+
+func TestCreateCasterResourceImages(t *testing.T) {
+	DefaultInstance.Client.SetAccessKey(testAk)
+	DefaultInstance.Client.SetSecretKey(testSk)
+	bodyMap := map[string]interface{}{
+		"CasterID": 0,
+		"Resource": map[string]interface{}{
+			"ID":   0,
+			"Name": "1",
+			"Type": 7,
+			"ImageCollection": []map[string]interface{}{
+				{
+					"ImageIndexID": 1,
+					"ImageID":      "testPic1",
+					"Name":         "LWScreenShot 2022-04-20 at 13.54.00.png",
+					"URL":          "http://test.example.com/testPic1.png",
+				},
+			},
+		},
+	}
+	body, _ := json.Marshal(bodyMap)
+	//打印请求参数
+	t.Logf(string(body))
+	resp, statusCode, err := DefaultInstance.CreateCasterResourceImages(nil, string(body))
+	if err != nil {
+		t.Logf("error occur %v", err)
+	}
+	res, _ := json.Marshal(resp)
+	t.Logf("statusCode = %+v  msgInfo = %+v \n", statusCode, string(res))
+}
+
+func TestUpdateCasterConfig(t *testing.T) {
+	DefaultInstance.Client.SetAccessKey(testAk)
+	DefaultInstance.Client.SetSecretKey(testSk)
+	bodyMap := map[string]interface{}{
+		"CasterID": 0,
+		"PushParams": map[string]interface{}{
+			"Direction": 1,
+			"Size":      1080,
+			"Fps":       30,
+			"VBitRate":  6000,
+			"Delay":     0,
+			"SEI":       "ts",
+			"Logo": map[string]interface{}{
+				"URL": "",
+				"X":   0,
+				"Y":   0,
+				"W":   0,
+				"H":   0,
+			},
+			"Gop":      2,
+			"ABitRate": 256,
+			"AChannel": "stereo",
+			"Codec":    "h264",
+			"Profile":  "high",
+			"ASample":  44100,
+			"Tune":     "",
+			"AILab":    "",
+			"Options":  "nal-hrd=cbr:force-cfr=1",
+			"Streams": []map[string]interface{}{
+				{
+					"ID":  1,
+					"URL": "rtmp://push.example.com/live/streamname",
+				},
+			},
+		},
+	}
+	body, _ := json.Marshal(bodyMap)
+	//打印请求参数
+	t.Logf(string(body))
+	resp, statusCode, err := DefaultInstance.UpdateCasterConfig(nil, string(body))
+	if err != nil {
+		t.Logf("error occur %v", err)
+	}
+	res, _ := json.Marshal(resp)
+	t.Logf("statusCode = %+v  msgInfo = %+v \n", statusCode, string(res))
+}
+
+func TestCreateCasterResourceOPED(t *testing.T) {
+	DefaultInstance.Client.SetAccessKey(testAk)
+	DefaultInstance.Client.SetSecretKey(testSk)
+	bodyMap := map[string]interface{}{
+		"CasterID": 0,
+		"Resource": map[string]interface{}{
+			"Name": "1",
+			"URL":  "http://test.example.com/open.mp4",
+		},
+	}
+	body, _ := json.Marshal(bodyMap)
+	//打印请求参数
+	t.Logf(string(body))
+	resp, statusCode, err := DefaultInstance.CreateCasterResourceOPED(nil, string(body))
+	if err != nil {
+		t.Logf("error occur %v", err)
+	}
+	res, _ := json.Marshal(resp)
+	t.Logf("statusCode = %+v  msgInfo = %+v \n", statusCode, string(res))
+}
+
+func TestDeleteCasterResourceOPED(t *testing.T) {
+	DefaultInstance.Client.SetAccessKey(testAk)
+	DefaultInstance.Client.SetSecretKey(testSk)
+	bodyMap := map[string]interface{}{
+		"CasterID":   0,
+		"ResourceID": 0,
+	}
+	body, _ := json.Marshal(bodyMap)
+	//打印请求参数
+	t.Logf(string(body))
+	resp, statusCode, err := DefaultInstance.DeleteCasterResourceOPED(nil, string(body))
+	if err != nil {
+		t.Logf("error occur %v", err)
+	}
+	res, _ := json.Marshal(resp)
+	t.Logf("statusCode = %+v  msgInfo = %+v \n", statusCode, string(res))
+}
