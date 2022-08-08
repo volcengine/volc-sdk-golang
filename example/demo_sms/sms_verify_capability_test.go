9a5869b6aeb04ca921cae24367a8af9e590a043d
chenxin <chenxin.shayne@bytedance.com>
2022-08-08 21:39:19 +0800

    sms demo update
    Change-Id: Ia41ec903185adaf316338167f0f5e001fc7f9446

---
+++ example/demo_sms/sms_verify_capability_test.go
@@ -0,0 +1,67 @@
+package demo_sms
+
+import (
+	"testing"
+
+	"github.com/volcengine/volc-sdk-golang/service/sms"
+)
+
+func TestSmsSendVerifyCode(t *testing.T) {
+	testAk := "testAK"
+	testSk := "testSk"
+
+	sms.DefaultInstance.Client.SetAccessKey(testAk)
+	sms.DefaultInstance.Client.SetSecretKey(testSk)
+
+	req := &sms.SmsVerifyCodeRequest{
+		SmsAccount:  "smsAccount",
+		Sign:        "sign",
+		TemplateID:  "ST_xxx",
+		PhoneNumber: "188xxxxxxxx",
+		Scene:       "myscene",
+		ExpireTime:  1800,
+		TryCount:    3,
+		CodeType:    6,
+		Tag:         "tag",
+	}
+	result, statusCode, err := sms.DefaultInstance.SendVerifyCode(req)
+	t.Logf("result = %+v\n", result)
+	t.Logf("statusCode = %+v\n", statusCode)
+	t.Logf("err = %+v\n", err)
+
+	if result == nil {
+		t.Logf("result is nil")
+		return
+	}
+	if result.ResponseMetadata.Error != nil {
+		t.Logf("result.ResponseMetadata.Error = %+v\n", result.ResponseMetadata.Error)
+		return
+	}
+	if result != nil && result.ResponseMetadata.Error == nil && result.Result != nil {
+		t.Logf("MessageID = %+v\n", result.Result.MessageID)
+	}
+}
+
+func TestSmsCheckVerifyCode(t *testing.T) {
+	testAk := "testAK"
+	testSk := "testSk"
+
+	sms.DefaultInstance.Client.SetAccessKey(testAk)
+	sms.DefaultInstance.Client.SetSecretKey(testSk)
+
+	req := &sms.CheckSmsVerifyCodeRequest{
+		SmsAccount:  "smsAccount",
+		PhoneNumber: "188xxxxxxxx",
+		Scene:       "myscene",
+		Code:        "111",
+	}
+	result, statusCode, err := sms.DefaultInstance.CheckVerifyCode(req)
+	/**
+	Result = "0" // OTP correct
+	Result = "1" // OTP error
+	Result = "2" // OTP expired
+	*/
+	t.Logf("result = %+v\n", result)
+	t.Logf("statusCode = %+v\n", statusCode)
+	t.Logf("err = %+v\n", err)
+}
