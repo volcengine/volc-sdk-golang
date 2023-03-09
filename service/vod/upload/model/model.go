ed1e7571bb66d41d22791abab0e9fb5783cebc27
雷懿行 <leiyixing@bytedance.com>
2023-03-09 11:43:03 +0000

    Vod
    * feat: 支持归档设置 content-type
    * Merge branch 'feature/support-vod-archive-upload-20230307' into 'vod'
    * Merge commit '30c91b061b5c39b9a6f2199fe18b5d4cd4390c98' into vod
    
    https://code.byted.org/volcengine/volc-sdk-golang/merge_requests/549

--- service/vod/upload/model/model.go
+++ service/vod/upload/model/model.go
@@ -6,9 +6,26 @@ type UploadPartCommonResponse struct {
 	Error   UploadPartError `json:"error"`
 }
 
-type UploadPartResponse struct {
+type InitPartResponse struct {
 	UploadPartCommonResponse
 	PayLoad InitPartPayLoad `json:"payload,omitempty"`
+}
+
+type UploadPartResponse struct {
+	UploadPartCommonResponse
+	PayLoad UploadPartPayLoad `json:"payload,omitempty"`
+}
+
+type Meta struct {
+	ObjectContentType string
+}
+
+type UploadPartPayLoad struct {
+	Uploadid   string `json:"uploadid"`
+	PartNumber string `json:"part_number"`
+	Crc32      string `json:"crc32"`
+	Etag       string `json:"etag"`
+	Meta       Meta   `json:"meta"`
 }
 
 type UploadMergeResponse struct {
@@ -32,7 +49,8 @@ type InitPartPayLoad struct {
 }
 
 type UploadPartCommon struct {
-	TosHost string
-	Oid     string
-	Auth    string
+	TosHost           string
+	Oid               string
+	Auth              string
+	ObjectContentType string
 }
