c1e1d8d08d5a4930d491d50fa67789bb952504f7
Hongrui Lian <lianhongrui.eric@bytedance.com>
2023-04-07 10:22:08 +0800

    fix: typo

--- example/imagex/describe_image_volc_cdn_access_log.go
+++ example/imagex/describe_image_volc_cdn_access_log.go
@@ -7,7 +7,7 @@ import (
 	"github.com/volcengine/volc-sdk-golang/service/imagex"
 )
 
-// 提取盲水印
+// 查询离线日志下载地址
 func main() {
 	// 默认 ImageX 实例为 `cn-north-1`，如果您想使用其他区域的实例，请使用 `imagex.NewInstanceWithRegion(区域名)` 显式指定区域
 	instance := imagex.NewInstance()
