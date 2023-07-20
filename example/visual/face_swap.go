3deb4865da678e9bab0b215f7194165957a79c0a
刘智会 <liuzhihui.0911@bytedance.com>
2023-07-20 12:37:45 +0000

    update facefusionmovie
    * update facefusionmovie
    * update faceswap
    
    https://code.byted.org/volcengine/volc-sdk-golang/merge_requests/689

--- example/visual/face_swap.go
+++ example/visual/face_swap.go
@@ -18,9 +18,26 @@ func main() {
 	//visual.DefaultInstance.SetHost("host")
 
 	form := url.Values{}
-	form.Add("image_base64", "")
-	form.Add("template_base64", "")
+	form.Add("image_base64", "xxx") //2.1版本无需此参数
+	//form.Add("image_url", "xxx") //2.1版本无需此参数
+	form.Add("template_base64", "xxx")
+	form.Add("template_url", "xxx")
 	form.Add("action_id", "faceswap")
+	form.Add("version", "") //1.0, 2.0, 2.1
+	form.Add("do_risk", "true")
+	form.Add("type", "") //仅限2.1版本
+	mergeInfoMap := map[string]interface{}{
+		//"image_base64": "/9xxx",
+		"image_url":         "https://xxx",
+		"location":          1,
+		"template_location": 1,
+	}
+	mergeInfo, err := json.Marshal(mergeInfoMap)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	form.Add("merge_infos", string(mergeInfo)) //仅限2.1版本
 
 	resp, status, err := visual.DefaultInstance.FaceSwap(form)
 	fmt.Println(status, err)
