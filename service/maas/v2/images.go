25b82fbb4f62f53d252e7a406b2d654416b6d366
何晓春 <hexiaochun@bytedance.com>
2024-04-25 12:09:25 +0000

    fix: add flex gen
    * fix: add flex gen
    * fix: add flex gen
    * fix: change parameters
    * fix: change parameters
    * fix: add plugin
    * fix: typo
    * Merge branch 'master' of https://code.byted.org/volcengine/volc-sdk-golang into…
    
    https://code.byted.org/volcengine/volc-sdk-golang/merge_requests/1008

--- service/maas/v2/images.go
+++ service/maas/v2/images.go
@@ -13,6 +13,8 @@ type images struct {
 	m *MaaS
 }
 
+// POST method
+// ImagesQuickGen
 func (i *images) ImagesQuickGen(endpointId string, req *api.ImagesQuickGenReq) (*api.ImagesQuickGenResp, int, error) {
 	return i.ImagesQuickGenWithCtx(context.Background(), endpointId, req)
 }
@@ -34,6 +36,36 @@ func (i *images) imagesQuickGenImpl(ctx context.Context, endpointId string, body
 	}
 
 	output := new(api.ImagesQuickGenResp)
+	if err = json.Unmarshal(respBody, output); err != nil {
+		return nil, status, api.NewClientSDKRequestError(fmt.Sprintf("failed to unmarshal response: %s", err.Error()), reqIdFromCtx(ctx))
+	}
+	output.ReqId = reqIdFromCtx(ctx)
+	return output, status, nil
+}
+
+// POST method
+// ImagesFlexGen
+func (i *images) ImagesFlexGen(endpointId string, req *api.ImagesReq) (*api.ImagesResp, int, error) {
+	return i.ImagesWithCtx(context.Background(), endpointId, req)
+}
+
+func (i *images) ImagesWithCtx(ctx context.Context, endpointId string, req *api.ImagesReq) (*api.ImagesResp, int, error) {
+	bts, err := json.Marshal(req)
+	if err != nil {
+		return nil, 0, api.NewClientSDKRequestError(fmt.Sprintf("failed to marshal request: %s", err.Error()), "")
+	}
+	return i.imagesImpl(ctx, endpointId, bts)
+}
+
+func (i *images) imagesImpl(ctx context.Context, endpointId string, body []byte) (*api.ImagesResp, int, error) {
+	ctx = getContext(ctx)
+
+	respBody, status, err := i.m.request(ctx, maas.APIImagesFlexGen, nil, endpointId, body, "")
+	if err != nil {
+		return nil, status, err
+	}
+
+	output := new(api.ImagesResp)
 	if err = json.Unmarshal(respBody, output); err != nil {
 		return nil, status, api.NewClientSDKRequestError(fmt.Sprintf("failed to unmarshal response: %s", err.Error()), reqIdFromCtx(ctx))
 	}
