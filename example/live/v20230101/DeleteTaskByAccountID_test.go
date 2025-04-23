a1039a5e601297cb26fe6d4188ec0d25ddf7e3b4
suqiangwei <suqiangwei@bytedance.com>
2025-04-23 14:20:09 +0800

    feat: 更新接口

---
+++ example/live/v20230101/DeleteTaskByAccountID_test.go
@@ -0,0 +1,31 @@
+package live_v20230101_test
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/volcengine/volc-sdk-golang/base"
+	"github.com/volcengine/volc-sdk-golang/service/live/v20230101"
+)
+
+func Test_DeleteTaskByAccountID(t *testing.T) {
+	instance := live_v20230101.NewInstance()
+
+	instance.SetCredential(base.Credentials{
+		AccessKeyID:     "ak",
+		SecretAccessKey: "sk",
+	})
+
+	param := &live_v20230101.DeleteTaskByAccountIDBody{}
+
+	resp, err := instance.DeleteTaskByAccountID(context.Background(), param)
+
+	if err != nil {
+		fmt.Printf("error %v", err)
+	} else {
+		t, _ := json.Marshal(resp)
+		fmt.Printf("success %v", string(t))
+	}
+}
