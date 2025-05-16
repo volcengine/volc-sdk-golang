250a6fb70cc904b1fd31b2a42a9d5bd8b2cb5e3b
zhangliang.dx <zhangliang.dx@bytedance.com>
2025-05-16 16:48:39 +0800

    imagex GetDomainOwnerVerifyContent、VerifyDomainOwner、GetImageHmExtractTaskInfo、CreateHmExtractTask等接口更新

---
+++ example/imagex/v2/api/CreateHmExtractTask_test.go
@@ -0,0 +1,34 @@
+package imagex_test
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/volcengine/volc-sdk-golang/base"
+	imagex "github.com/volcengine/volc-sdk-golang/service/imagex/v2"
+)
+
+func Test_CreateHmExtractTask(t *testing.T) {
+	instance := imagex.NewInstance()
+
+	instance.SetCredential(base.Credentials{
+		AccessKeyID:     "ak",
+		SecretAccessKey: "sk",
+	})
+
+	param := &imagex.CreateHmExtractTaskReq{
+		CreateHmExtractTaskQuery: &imagex.CreateHmExtractTaskQuery{},
+		CreateHmExtractTaskBody:  &imagex.CreateHmExtractTaskBody{},
+	}
+
+	resp, err := instance.CreateHmExtractTask(context.Background(), param)
+
+	if err != nil {
+		fmt.Printf("error %v", err)
+	} else {
+		t, _ := json.Marshal(resp)
+		fmt.Printf("success %v", string(t))
+	}
+}
